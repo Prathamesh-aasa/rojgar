ff481f0ca9cb96d53ecd0d41b8f4fd2fd7668903
aasa-vikas <vikas.patel@aasa.tech>
2024-06-22 14:34:17 +0530

    code sync

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "hsla(208, 83%, 37%, 1)",
+        secondary: "hsla(208, 83%, 37%, 1)",
+       
+      },
+    },
+  },
+  plugins: [],
+};
